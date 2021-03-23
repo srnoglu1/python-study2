2324b067e8050ba07287e4301923e1a9d71a5fcc
emirhan sirinoglu <srnoglu98@gmail.com>
2021-03-23 22:30:09 +0300

    global-and-local-variables

---
+++ scope.py
@@ -0,0 +1,43 @@
+x = 'global x'
+
+def function():
+    #local scope 
+    x = 'local x'
+    print(x)
+
+function()
+
+################
+
+name = 'Emir'
+
+def changeName(newName):
+    #local
+    name = newName
+    print(name)
+changeName('Ada')
+print(name)
+
+################
+
+name = 'global string'
+
+def greeting():
+    #name = 'Emir'
+    def hello():
+        #name = 'Ada'
+        print(f'hello ' + name)
+    hello()
+greeting()
+
+################
+
+x = 50
+def test(x):
+    global x
+    print(f'x : {x}') 
+
+    x = 100
+    print(f'changed x to {x}' )
+test()
+print(x)
