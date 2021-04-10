3c732f1040f13813c57b48460b38ad5c3cbd573f
emirhan sirinoglu <srnoglu98@gmail.com>
2021-04-10 22:51:59 +0300

    error-handling

--- error.py
+++ error.py
@@ -8,3 +8,22 @@ try:
     print("Entered values are successful.")
 except:
     print("incorrect value entered.")
+
+#
+
+while True:
+    try:
+        x = int(input('x: '))
+        y = int(input('y: '))
+        print(x/y)
+    except (ZeroDivisionError,ValueError) as e:
+        print('an error occorred.')
+        print(e)
+    except Exception as e:
+        print('an uncnown error has occurred.')
+        print(e)
+    else:
+        break
+    finally:
+        print('finally block worked.')
+    
